0be8a34568f62f7665ec5f04b2d9101fa04e4a53
ponderwa <ponderwa@163.com>
2017-12-18 20:51:56 +0800

    Site updated: 2017-12-18 20:51:38

--- js/myscript.js
+++ js/myscript.js
@@ -1,24 +1,23 @@
-function test() {
-    var haha = document.getElementById("testt");
-    var hahaha = document.getElementById("testtt");
-    var scrollTop = document.documentElement.scrollTop || window.pageYOfset ||document.body.scrollTop;
+window.onload=myfun;
+function myfun()
+{
+    var btn = document.getElementById("float-btn");
+    btn.style.display = "block";
+    btn.style.display = "none";
+}
 
+window.onscroll = function () {
+    var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+    var btn = document.getElementById("float-btn");
 
+    console.log(scrollTop);
 
-
-        if (haha.style.display == 'none') {
-            haha.style.display = 'block';
-            hahaha.style.display = 'block';
-        }
-        else {
-            haha.style.display = 'none';
-            hahaha.style.display = 'none';
-        }
-    // alert("是的，这是一个骗局！");
-
-    // alert(scrollTop); //获取滚动条到顶部的垂直高度
-    // alert($(window).width()); //浏览器时下窗口可视区域宽度
-    // alert($(document).width());//浏览器时下窗口文档对于象宽度
+    if (scrollTop > 700) {
+        btn.style.display = "block";
+    }
+    else {
+        btn.style.display = "none";
+    }
 }
 
 
@@ -37,3 +36,53 @@ function resizeBannerImage()
         haha.style.display = 'block';
     }
 }
+
+function test() {
+    var haha = document.getElementById("testt");
+    var hahaha = document.getElementById("testtt");
+
+    if (haha.style.display == 'none') {
+        haha.style.display = 'block';
+        hahaha.style.display = 'block';
+    }
+    else {
+        haha.style.display = 'none';
+        hahaha.style.display = 'none';
+    }
+    // alert("是的，这是一个骗局！");
+    // alert(scrollTop); //获取滚动条到顶部的垂直高度
+    // alert($(window).width()); //浏览器时下窗口可视区域宽度
+    // alert($(document).width());//浏览器时下窗口文档对于象宽度
+}
+
+
+
+
+// window.addEventListener("scroll", function() {
+//     // var flag = false;
+//
+//
+//     console.log(scrollTop);
+//     var btn = document.getElementById("float-btn");
+//
+//
+//     // btn.style.display = "block"
+//
+//     if(scrollTop >= 1000) {
+//         // btn.style.display = "";
+//         btn.style.opacity = 1 ;
+//         btn.style.visibility ="visible";
+//
+//     }else{
+//         // btn.style.display = "none";
+//         btn.style.visibility ="hidden";
+//     }
+// });
+
+
+
+
+
+
+
+
